c1c11a8d0291a526daa8b84c3094e89239d71f0d
aduckworth1969 <duckworth.andy@gmail.com>
2020-05-28 19:52:36 -0700

    Added a restart function

--- Notes.py
+++ Notes.py
@@ -1,25 +1,26 @@
 import os
 from datetime import date
 
+def file_process():
 # variable for time format
-today = date.today()
-date_append = today.strftime("%Y%m%d")
+	today = date.today()
+	date_append = today.strftime("%Y%m%d")
 
-change_directory = input("Type the path to your directory: ")
+	change_directory = input("Type the path to your directory: ")
 
 # changes the working directory to Notes in icloud
-os.chdir(change_directory)
+	os.chdir(change_directory)
 
 # user input to define variables for tags
-begin_tag = input("What is the beginning tag? > ")
-con_tag = "E"
-end_tag = con_tag + begin_tag
+	begin_tag = input("What is the beginning tag? > ")
+	con_tag = "E"
+	end_tag = con_tag + begin_tag
 
 # user input to define variable for file to open
-file_to_open = input("What file do you want to open? > ")
+	file_to_open = input("What file do you want to open? > ")
 
 # user input to define variable for file to save
-file_to_save = input("What do you want to call the save file? > ")
+	file_to_save = input("What do you want to call the save file? > ")
 
 # reads the file that was identified via the variable from user input
 # read_file = open(file_to_open, 'r')
@@ -27,14 +28,26 @@ file_to_save = input("What do you want to call the save file? > ")
 # pulls text out of the file as a variable
 # my_file_contents = read_file.read()
 
-with open(file_to_open) as infile, open(file_to_save + date_append + ".md", 'w') as outfile:
-    copy = False
-    for line in infile:
-        if line.strip() == begin_tag:
-            copy = True
-            continue
-        elif line.strip() == end_tag:
-            copy = False
-            continue
-        elif copy:
-            outfile.write(line)
+# opens file for processing and saves file
+	with open(file_to_open) as infile, open(file_to_save + date_append + ".md", 'w') as outfile:
+    		copy = False
+    		for line in infile:
+        		if line.strip() == begin_tag:
+            			copy = True
+            			continue
+        		elif line.strip() == end_tag:
+            			copy = False
+            			continue
+       			elif copy:
+            			outfile.write(line)
+
+file_process()
+
+def restart():
+# program code here...
+	restart = input("Do you have more tags to process? ")
+	if restart == "yes" or restart == "y":
+		file_process()
+	if restart == "n" or restart == "no":
+		print("Script terminating. Goodbye.")
+restart()
