201bfa9557ea1f5bdfa0b16f7f840aa5db5e418f
aduckworth1969 <duckworth.andy@gmail.com>
2020-06-30 15:04:30 -0700

    Embedded ConvertWord.py into Notes.py

--- Notes.py
+++ Notes.py
@@ -2,6 +2,7 @@ import sys
 import os
 from datetime import date
 from sys import exit
+import pypandoc
 
 # menu for application
 def notes_menu():
@@ -9,7 +10,8 @@ def notes_menu():
     print("\n1. Set directory.\n")
     print("2. Add tags.\n")
     print("3. Process file.\n")
-    print("4. Quit.\n")
+    print("4. Convert markdown to Word.\n")
+    print("5. Quit.\n")
     print("________________________\n")
     menu_selection = input("Make selection:> ")
     if menu_selection == "1":
@@ -19,9 +21,11 @@ def notes_menu():
     elif menu_selection == "3":
         file_process()
     elif menu_selection == "4":
+        convert_word()
+    elif menu_selection == "5":
         exit(0)
     else:
-        print(tag_list)
+        exit(0)
 
 def set_directory():
     current_dir = os.getcwd()
@@ -49,13 +53,14 @@ def add_tags():
         notes_menu()
 
 def file_process():
+
+# user input for file to open
+    file_to_open = input("What file do you want to open? > ")
+
 # user input for tags
     begin_tag = input("Start tag:> ")
     con_tag = "E"
     end_tag = con_tag + begin_tag
-
-# user input for file to open
-    file_to_open = input("What file do you want to open? > ")
 
 # user input for file to save
     file_to_save = input("Save file:> ")
@@ -72,14 +77,23 @@ def file_process():
                 continue
             elif copy:
                 outfile.write(line)
-    restart()
 
-def restart():
     restart = input("Do you have more tags to process? ")
     if restart == "yes" or restart == "y":
-        file_process()
+        file_extract()
     if restart == "n" or restart == "no":
         print("Have a great day!. Goodbye.")
+        notes_menu()
+
+def convert_word():
+    input_file = input("File to convert:> ")
+    output_file = input("Save file:> ")
+    output = pypandoc.convert_file(input_file, 'docx', outputfile=output_file + date_append + ".docx")
+    assert output == ""
+    more_files = input("Convert another file? ")
+    if more_files == "yes" or more_files == "y":
+        convert_word()
+    if more_files == "no" or more_files == "n":
         notes_menu()
 
 # variable for time format
