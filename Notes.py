7cd11c86d372db7957fca56d168d27ae91405b1a
aduckworth1969 <duckworth.andy@gmail.com>
2020-06-30 17:59:18 -0700

    Embedded Notion upload function to Notes.py

--- Notes.py
+++ Notes.py
@@ -3,6 +3,9 @@ import os
 from datetime import date
 from sys import exit
 import pypandoc
+from notion.client import NotionClient
+from md2notion.upload import upload
+from notion.block import PageBlock
 
 # menu for application
 def notes_menu():
@@ -11,7 +14,8 @@ def notes_menu():
     print("2. Add tags.\n")
     print("3. Process file.\n")
     print("4. Convert markdown to Word.\n")
-    print("5. Quit.\n")
+    print("5. Upload to Notion.\n")
+    print("6. Quit.\n")
     print("________________________\n")
     menu_selection = input("Make selection:> ")
     if menu_selection == "1":
@@ -23,6 +27,8 @@ def notes_menu():
     elif menu_selection == "4":
         convert_word()
     elif menu_selection == "5":
+        notion_upload()
+    elif menu_selection == "6":
         exit(0)
     else:
         exit(0)
@@ -94,6 +100,21 @@ def convert_word():
     if more_files == "yes" or more_files == "y":
         convert_word()
     if more_files == "no" or more_files == "n":
+        notes_menu()
+
+def notion_upload():
+    notion_page = input("Notion page URL:> ")
+    file_upload = input("File:> ")
+    file_title = input("Page title:> ")
+    client = NotionClient(token_v2="1082148eb4f64df07b7e7e3abde9615282978abcbc0724769bf05548243e304164bf94bfd110c1646930be92239dc75c72bb61ff15f6888a0454958a983484ce5c3f52ea7048feaf3f5aa9025b47")
+    page = client.get_block(notion_page)
+    with open(file_upload, 'r', encoding='utf-8') as mdfile:
+        newPage = page.children.add_new(PageBlock, title=file_title)
+        upload(mdfile, newPage) # appends the converted contents fo markdown file to new page
+    new_upload = input("Upload new file?> ")
+    if new_upload == "yes" or new_upload == "y":
+        notion_upload()
+    if new_upload == "no" or new_upload == "n":
         notes_menu()
 
 # variable for time format
